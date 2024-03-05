fcd3a3e21a74177492964c941a9db85013322b2b
Rajesh kumar jena <129972543+Rajesh-1234567@users.noreply.github.com>
2024-03-05 03:48:38 +0000

    conversion done

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,15 @@
+let score=33
+console.log(typeof score)
+console.log(typeof(score))
+let valueNumber = Number(score)
+console.log(typeof(valueNumber))
+let anyNumber;
+console.log(anyNumber)
+let anotherScore = null
+console.log(anotherScore)
+console.log(Number(anotherScore))
+let str= "Rajesh"
+console.log(typeof(str))
+console.log(Number(str))
+let first = true
+console.log(typeof(first))
